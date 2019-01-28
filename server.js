45de14f970912b862789853cdb2baca5c2a95f20
Nikhil Singh <nikhilsingh498@gmail.com>
2019-01-28 11:06:40 +0530

    Test update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const video = require('./video.js');
+var youtubedl = require('youtube-dl');
+// const video = require('./video.js');
 
 
 const port =3000;
@@ -15,26 +16,35 @@ app.get('/', (req,res) => {
     res.render('index.hbs');
 });
 
-
-
-
 app.get('/download', (req,res) => {
+    var video = youtubedl(req.query.video);
+    video.on('info', function(info) {
+        console.log('Download started');
+        console.log('filename: ' + info._filename);
+        console.log('size: ' + info.size);
+    });
+
+    video.pipe(fs.createWriteStream('myvideo.mp4'));
+});
+
+
+// app.get('/download', (req,res) => {
     
     //var myText= req.query.video;
     //module.exports.URL=myText;
    //const video = require('./video.js');
-    video.getVideo(req.query.video,(error,video) => {
-        if(error){
-            res.render('error.hbs' ,{
-                error:error
-            
-            })
-        }
-    else{
-        console.log(video.items);
-    }
-})
-});
+   //  video.getVideo(req.query.video,(error,video) => {
+   //      if(error){
+   //          res.render('error.hbs' ,{
+   //              error : "Page "
+   //
+   //          })
+   //      }
+   //  else{
+   //      console.log(req.query.video);
+   // }
+// // })
+// });
 
 
 
--- video.js
+++ video.js
@@ -1,42 +1,56 @@
-const request = require("request");
-// const id=require('./server.js');
-//  var myText=id.URL;
+// const request = require("request");
+// // const id=require('./server.js');
+// //  var myText=id.URL;
+//
+//
+// //var url ='https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ID+'&key=AIzaSyDXom2ZGtlbb7yGtb4cxsDYxStryzITbXY'
+//
+// var getVideo=(URL,callback)=> {
+//   var ID = extractVideoID(URL);
+// request({
+//     url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ID+'&key=AIzaSyDXom2ZGtlbb7yGtb4cxsDYxStryzITbXY',
+//     json: true
+// }, function (error, response, body) {
+//
+//     if (!error) {
+//            callback(undefined,{
+//                items:body.items
+//            })
+//     }
+//     else
+//     {
+//         callback("No internet Connection");
+//     }
+//
+//
+// });
+// };
+//
+//
+// function extractVideoID(url){
+//     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+//     var match = url.match(regExp);
+//     if ( match && match[7].length == 11 ){
+//         return match[7];
+//     }else{
+//         console.log("Could not extract video ID.");
+//     }
+// }
+//
+// module.exports.getVideo =getVideo;
 
 
-//var url ='https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ID+'&key=AIzaSyDXom2ZGtlbb7yGtb4cxsDYxStryzITbXY'
- 
-var getVideo=(URL,callback)=> {
-  var ID=extractVideoID(URL);
-request({
-    url: 'https://www.googleapis.com/youtube/v3/videos?part=snippet&id='+ID+'&key=AIzaSyDXom2ZGtlbb7yGtb4cxsDYxStryzITbXY',
-    json: true
-}, function (error, response, body) {
+var fs = require('fs');
+var youtubedl = require('youtube-dl');
+var video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA');
 
-    if (!error) {
-           callback(undefined,{
-               items:body.items
-           })
-    }
-    else
-    {
-        callback("No internet Connection");
-    }
-    
-
+// Will be called when the download starts.
+video.on('info', function(info) {
+    console.log('Download started');
+    console.log('filename: ' + info.filename);
+    console.log('size: ' + info.size);
 });
-};
 
-
-function extractVideoID(url){
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-    var match = url.match(regExp);
-    if ( match && match[7].length == 11 ){
-        return match[7];
-    }else{
-        console.log("Could not extract video ID.");
-    }
-}
-
-module.exports.getVideo =getVideo;
+video.pipe(fs.createWriteStream('myvideo.mp4'));
 
 
